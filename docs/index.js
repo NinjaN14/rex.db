99f0a110c72677e6f688e3351c976d35b175f341
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-31 12:04:24 +0545

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -3,8 +3,29 @@ var markdown = `
 
 *If you're having troubles installing, please follow [this troubleshooting guide](https://github.com/JoshuaWise/better-sqlite3/blob/master/docs/troubleshooting.md).*
 
-**Linux & Windows**
+**Windows**
 - \`npm i better-quickdb\`
+- Error?
+    1. Install the **latest** of node 10 or node 12.
+    2. Start an Admin PowerShell: Right-click the start icon, then pick \`Windows PowerShell (Admin)\`
+    3. Install *both* vs2015 and vs2017 libraries. Each line will take ~5-10 minutes.
+       \`\`\`sh
+       npm install --global --production --vs2015 --add-python-to-path windows-build-tools
+       npm install --global --production --add-python-to-path windows-build-tools node-gyp
+       \`\`\`
+    4. In your project, make sure you're not fighting with old build configurations. Delete **both**
+       * your \`%USERPROFILE%/.node-gyp\` and
+       * your project's \`node_modules\` directories.
+    5. Set up \`%USERPROFILE%/.npmrc\` correctly:
+       \`\`\`sh
+       msvs_version=2015
+       python=python2.7
+       \`\`\`
+       (where \`%USERPROFILE%\` is your home directory).
+    6. Run \`npm install better-quickdb\`
+
+**Linux**
+- `npm i better-quickdb`
 
 ***Note:** Windows users may need to do additional steps [listed here](https://github.com/JoshuaWise/better-sqlite3/blob/master/docs/troubleshooting.md).*
 
